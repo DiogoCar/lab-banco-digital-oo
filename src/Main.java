59aad8890d441151e024b9fc594386939a249543
Diogo Carneiro <141922211+DiogoCar@users.noreply.github.com>
2024-02-11 16:20:37 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,11 @@ public class Main {
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
+
+        cc = Math.floor(n * 100) / 100;
+        
+        if (cc == 0.00) {System.out.println("=== ERRO ==="+"\n"+"Valor de deposito não pode ser 0!");return;}
+        else if (cc < 0.00) {System.out.println("=== ERRO ==="+"\n"+"Valor de deposito não pode ser negativo!");return;}
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
